a5e3cae0ab1405777b4c2f890c36a8657c50de5a
Roger Zavala <rogeliozavala15@gmail.com>
2021-01-23 12:56:17 -0800

    new methods

--- main.js
+++ main.js
@@ -4,3 +4,38 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field{
+    constructor(field){
+        this._field = field;
+    }
+
+    get field(){
+        return this._field;
+    }
+
+    print(){
+        
+        const displayString = this.field.map(row => {
+            return row.join('');
+        }).join('\n');
+        console.log(displayString);
+    }
+
+    askQuestion(){
+        
+    }
+
+    static generateField(){
+        
+    }
+}
+
+
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+  myField.print();
